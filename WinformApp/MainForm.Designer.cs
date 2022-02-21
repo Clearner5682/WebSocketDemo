ea8c126e4327c06b50d52a7e061587dc6d0afcb3
洪岩 <dannyhong@rektec.com.cn>
2022-02-21 15:41:38 +0800

    增加Winform客户端建立WebSocket连接的Demo

---
+++ WinformApp/MainForm.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace WinformApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtServerUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtContent = new System.Windows.Forms.RichTextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtServerUrl
+            // 
+            this.txtServerUrl.Location = new System.Drawing.Point(173, 28);
+            this.txtServerUrl.Name = "txtServerUrl";
+            this.txtServerUrl.Size = new System.Drawing.Size(249, 23);
+            this.txtServerUrl.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "WebSocket服务器地址：";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(459, 24);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(81, 30);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "建立连接";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(459, 24);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(81, 30);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "断开连接";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(21, 70);
+            this.txtContent.Name = "txtContent";
+            this.txtContent.ReadOnly = true;
+            this.txtContent.Size = new System.Drawing.Size(742, 322);
+            this.txtContent.TabIndex = 4;
+            this.txtContent.Text = "";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(21, 486);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(81, 30);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(21, 418);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(742, 43);
+            this.txtInput.TabIndex = 6;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 541);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtServerUrl);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtServerUrl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.RichTextBox txtContent;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtInput;
+    }
+}
+
---
+++ WinformApp/MainForm.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.WebSockets;
+using System.Threading;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace WinformApp
+{
+    public partial class MainForm : Form
+    {
+        private ClientWebSocket client = null;
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.btnClose.Hide();
+            this.btnSend.Enabled = false;
+        }
+
+        // 建立连接
+        private async void btnConnect_Click(object sender, EventArgs e)
+        {
+            this.txtServerUrl.Enabled = false;
+            Uri serverUrI =new Uri(this.txtServerUrl.Text);
+            try
+            {
+                client = new ClientWebSocket();
+                client.Options.Cookies = new CookieContainer();
+                client.Options.Cookies.Add(new Cookie("UserId","wulinhao","/","localhost"));
+                await client.ConnectAsync(serverUrI, CancellationToken.None);
+                // 连接成功了
+                this.btnConnect.Hide();
+                this.btnClose.Show();
+                this.btnSend.Enabled = true;
+            }
+            catch(Exception ex)
+            {
+                this.txtServerUrl.Enabled = true;
+                this.btnConnect.Show();
+                this.btnClose.Hide();
+                this.btnSend.Enabled = false;
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            KeepReceiving(client);
+        }
+
+        // 保持接收消息
+        public async void KeepReceiving(ClientWebSocket client)
+        {
+            // 设置一个10K的缓冲区
+            byte[] buffer = new byte[10 * 1024];
+            IList<byte> byteList = new List<byte>();
+            while (true)
+            {
+                buffer = new byte[10 * 1024];
+                var result =await client.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                if (result.CloseStatus.HasValue)
+                {
+                    // 收到了关闭连接的消息，断开连接
+                    // 被动关闭连接
+                    CloseConnection(client, result.CloseStatus.Value, result.CloseStatusDescription);
+                    break;
+                }
+
+                for (int i = 0; i < result.Count; i++)
+                {
+                    byteList.Add(buffer[i]);
+                }
+
+                if (result.EndOfMessage)
+                {
+                    // 一条消息接收完了
+                    var jsonString = Encoding.UTF8.GetString(byteList.ToArray());
+                    var messageInfo = JsonConvert.DeserializeObject<MessageInfo>(jsonString);
+                    this.BeginInvoke(new Action<MessageInfo>(ShowMessage),messageInfo);
+
+                    byteList.Clear();
+                }
+            }
+        }
+
+        public async Task CloseConnection(ClientWebSocket client,WebSocketCloseStatus closeStatus=WebSocketCloseStatus.NormalClosure,string statusDescription="")
+        {
+            await client.CloseAsync(closeStatus, statusDescription, CancellationToken.None);
+            client.Dispose();
+            this.btnConnect.Show();
+            this.btnClose.Hide();
+            this.btnSend.Enabled = false;
+        }
+
+        private void ShowMessage(MessageInfo messageInfo)
+        {
+            var fromUser = messageInfo.FromUser;
+            var message = messageInfo.Message;
+            this.txtContent.AppendText(fromUser + ":" + message + "\r\n");
+        }
+
+        // 发送消息
+        private async void btnSend_Click(object sender, EventArgs e)
+        {
+            var message = this.txtInput.Text;
+            if (string.IsNullOrEmpty(message))
+            {
+                MessageBox.Show("发送的消息不能为空");
+                return;
+            }
+            MessageInfo messageInfo = new MessageInfo
+            {
+                FromUser="wulinhao",
+                Message=message,
+                IsToAll=true
+            };
+            await Send(messageInfo);
+        }
+
+        private async Task Send(MessageInfo messageInfo)
+        {
+            var jsonString = JsonConvert.SerializeObject(messageInfo);
+            byte[] bytes = Encoding.UTF8.GetBytes(jsonString);
+            if (client != null && client.State == WebSocketState.Open)
+            {
+                await client.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, CancellationToken.None);
+            }
+            else
+            {
+                MessageBox.Show("WebSocket未连接");
+                return;
+            }
+        }
+
+        private async void btnClose_Click(object sender, EventArgs e)
+        {
+            await CloseConnection(client);
+        }
+    }
+
+    public class MessageInfo
+    {
+        public string FromUser { get; set; }
+        public string Message { get; set; }
+        public bool IsToAll { get; set; }
+        public string ToUser { get; set; }
+    }
+}
